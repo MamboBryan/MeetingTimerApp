4d11bc6f0b0a7f78f4126a5abf4fb77cf22bbeef
MamboBryan <mambobryan@gmail.com>
2019-08-31 20:52:34 +0300

    [Finishes #168233869] Creates landing page and home actvity

---
+++ app/src/main/java/com/mambobryan/jwtimer/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.mambobryan.jwtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
---
+++ app/src/main/java/com/mambobryan/jwtimer/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.mambobryan.jwtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Splash Screen Timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myIntent;
+                myIntent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
